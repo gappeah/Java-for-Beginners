2030d45740c17da76c81e15c7ee480f9aba09954
George <appeahg@outlook.com>
2024-06-08 18:02:07 +0100

    Updated project file

---
+++ Largest Number.java
@@ -0,0 +1,40 @@
+// Question on sorting the largest number in 3 numbers
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your first whole number: ");
+		int a = scanner.nextInt();
+		
+		System.out.println("Enter your second whole number: ");
+		int b = scanner.nextInt();
+		
+		System.out.println("Enter your third whole number: ");
+		int c = scanner.nextInt();
+		
+		int max = a;
+		if (max < b) {
+			max = b;
+		}
+		
+		if (max < c) {
+			max = c;
+			
+		}
+		System.out.println(max);
+		
+				
+			
+		}
+		
+		
+		
+		}
+	
+
+		
+	
