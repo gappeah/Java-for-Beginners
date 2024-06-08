3c51108eb3833ffaa979219eb1a3d908f0ccbc72
George <114095068+gappeah@users.noreply.github.com>
2024-06-08 18:27:03 +0100

    Update Alphabet case check.java

--- Alphabet case check.java
+++ Alphabet case check.java
@@ -1,1 +1,37 @@
+// Alphabet case check
 
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your first string: ");
+		char ch  = scanner.next().trim().charAt(0);
+		//scanner.nextLine
+		if(Character.isLowerCase(ch)) {
+			System.out.println(ch + " is a lower case letter");
+			
+		} else {
+				
+			System.out.println(ch + " is a upper case letter");
+			}
+			
+			
+			
+		}
+		
+				
+			
+		
+		
+		
+		
+		}
+	
+
+		
+	
+
+	
