b6c9cb8861a45049f4ec5828b46e554ad1e54f4c
George <114095068+gappeah@users.noreply.github.com>
2024-06-08 19:17:07 +0100

    Update Fibonacci sequence.java

--- Fibonacci sequence.java
+++ Fibonacci sequence.java
@@ -1,1 +1,55 @@
 // fibonacci sequence
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner scanner = new Scanner(System.in);
+				 
+		 
+		System.out.println("Type a number? ");
+		int n = scanner.nextInt();
+	
+		int a = 0;
+		int b = 1;
+		
+		int count = 2;
+		while (count <= n) {
+			
+			int temp = b;
+			
+			b = a + b;
+			
+			
+			a = temp;
+			count++;
+			
+			
+			
+			System.out.println(b);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+		}
+		
+				
+			
+		
+		
+		
+		
+		}
+	
+
+		
+	
