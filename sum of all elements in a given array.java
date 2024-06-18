9f2ae1e0fde78a83fb0570ad86d283dd65e99d1d
George <114095068+gappeah@users.noreply.github.com>
2024-06-18 20:05:20 +0100

    Create sum of all elements in a given array.java

---
+++ sum of all elements in a given array.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) { 
+	  int numbers[] =  {1, 3 ,5 ,7 ,9};
+	  
+	  int sum = 0;	  
+	  
+	   for (int number:numbers) {
+		   sum += number;
+		  
+	   }
+			  
+	  System.out.println(sum);
+	  
+	  }
+	  	  
+	 
+	  }
+		  
+  
+	  
+	  
+	  
+  
