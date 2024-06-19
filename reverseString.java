de8285cb03a6f897757ea510079592a3685230a4
George <114095068+gappeah@users.noreply.github.com>
2024-06-19 16:35:07 +0100

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a string text: ");// User writes a string 
+        String text = scanner.nextLine();
+        int strlength = text.length();        // Get string length
+
+        while(strlength > 0);{
+        	
+        	
+        	
+        	
+        	System.out.print(text.charAt(strlength -1));
+        	
+        	strlength--;
+        }
+        
+        
+        
+        
+        
+      
+        }
+    }
 
