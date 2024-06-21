93f7dc3bd6685094dcb19db055fe7cf60e4bc519
George <114095068+gappeah@users.noreply.github.com>
2024-06-21 15:07:09 +0100

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,28 +1,37 @@
-import java.util.Scanner;
+//Write a Java program to reverse a given string.*/
+import java.util.Scanner; // Imports the scanner object from java.util
 
 public class Main {
     public static void main(String[] args) { 
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter a string text: ");// User writes a string 
-        String text = scanner.nextLine();
-        int strlength = text.length();        // Get string length
+    	
+    	String original = "", reversed = ""; 
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	while(!original.contains("Exit"))
+        
+    	{
+    	
+        original = "";
+        reversed = "";
+        System.out.println("Enter a string for which you want to be reversed: ");// User writes a string 
+        //String text = scanner.nextLine(); // Gets stored 
+        original = scanner.nextLine(); // The input from the user is stored 
+        int length = original.length(); //
+        int i;
 
-        while(strlength > 0);{
-        	
-        	
-        	
-        	
-        	System.out.print(text.charAt(strlength -1));
-        	
-        	strlength--;
+        for(i = length - 1 ; i >= 0; i--) {
+        reversed = reversed + original.charAt(i);
+        System.out.println("Reversed string: " + reversed);
+
+        }
         }
         
         
-        
-        
-        
-      
         }
     }
+
+  
+	  
+	  
 
